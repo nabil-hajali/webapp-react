779c601d8a4f911643cf9d8178b37e94166bf728
nabil-hajali <nabil2491@gmail.com>
2026-04-28 22:32:16 +0200

    millestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import  DefaultLayout  from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
+import MoviePage from "./pages/MoviePage"
 
 
 function App() {
 
   return (
     <>
-      
+
+      <BrowserRouter>
+        <Routes>
+
+          <Route element={<DefaultLayout />} >
+
+            <Route index element={<HomePage/>} />
+            <Route path="/movies/:movieId" element={<MoviePage />} />
+
+          </Route>
 
 
+        </Routes>
 
+
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout() {
+    return (
+
+        <>
+            <header>Header</header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>Footer</footer>
+        </>
+    )
+}
